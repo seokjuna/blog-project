198a70bd900a28bf38e04d8a16b7fab919d65b8c
naseokju <lukas95@naver.com>
2022-11-14 18:25:25 +0900

    blog-backend

--- blog-backend/src/api/posts/index.js
+++ blog-backend/src/api/posts/index.js
@@ -1,14 +1,16 @@
-const Router = require('koa-router');
-const postsCtrl = require('./posts.ctrl');
+import Router from 'koa-router';
+import * as postsCtrl from './posts.ctrl';
 
 const posts = new Router();
 
-
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.read);
-posts.delete('/:id', postsCtrl.remove);
-posts.put('/:id', postsCtrl.replace);
-posts.patch('/:id', postsCtrl.update);
 
-module.exports = posts;
+const post = new Router(); // /api/posts/:id
+posts.get('/', postsCtrl.read);
+posts.delete('/', postsCtrl.remove);
+posts.patch('/', postsCtrl.update);
+
+posts.use('/:id', postsCtrl.checkObjectId, post.routes());
+
+export default posts;
