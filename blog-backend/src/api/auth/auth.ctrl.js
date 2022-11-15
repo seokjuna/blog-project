50e28cf940f5bf0a75763c4935fe176f6ddeff4e
naseokju <lukas95@naver.com>
2022-11-15 16:12:46 +0900

    blog-backend

---
+++ blog-backend/src/api/auth/auth.ctrl.js
@@ -0,0 +1,89 @@
+import Joi from "../../../node_modules/joi/lib/index";
+import User from "../../models/user";
+
+/* 회원가입
+POST /api/auth/register
+{
+    username: 'naseokju',
+    password: 'pass111'
+}
+*/
+export const register = async ctx => {
+    // Request Body 검증하기
+    const schema = Joi.object().keys({
+        username: Joi.string()
+            .alphanum()
+            .min(3)
+            .max(20)
+            .required(),
+        password: Joi.string().required(),
+    });
+    const result = schema.validate(ctx.request.body);
+    if (result.error) {
+        ctx.status = 400;
+        ctx.body = result.error;
+        return;
+    }
+    
+    const { username, password } = ctx.request.body;
+    try {
+        // username이 이미 존재하는지 확인
+        const exists = await User.findByUsername(username);
+        if (exists) {
+            ctx.status = 409; // Conflict
+        }
+
+        const user = new User({
+            username,
+        });
+        await user.setPassword(password); // 비밀번호 설정
+        await user.save(); // db에 저장
+
+        ctx.body = user.serialize();
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+};
+
+/* 로그인
+POST /api/auth/login
+{
+    username: 'naseokju123',
+    password: 'pass123'
+}
+*/
+export const login = async ctx => {
+    const { username, password } = ctx.request.body;
+
+    // username, password가 없으면 에러 처리
+    if (!username || !password) {
+        ctx.status = 401; // Unauthroized
+        return;
+    }
+
+    try {
+        const user = await User.findByUsername(username);
+        // 계정이 존재하지 않으면 에러 처리
+        if (!user) {
+            ctx.status = 401;
+            return;
+        }
+        const valid = await user.checkPassword(password);
+        // 잘못된 비밀번호
+        if(!valid) {
+            ctx.status = 401;
+            return;
+        }
+        ctx.body = user.serialize();
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+};
+
+export const check = async ctx => {
+    // 로그인 상태 확인
+};
+
+export const logout = async ctx => {
+  ;  // 로그아웃
+};
--- blog-backend/src/api/index.js
+++ blog-backend/src/api/index.js
@@ -1,9 +1,11 @@
 import Router from 'koa-router';
 import posts  from './posts';
+import auths from './auth/index';
 
 const api = new Router();
 
 api.use('/posts', posts.routes());
+api.use('/auth', auths.routes());
 
 // 라우터를 내보냄
 export default api;
