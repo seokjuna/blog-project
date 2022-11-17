7ed14d9a26685af959352c79a1bd92652d81524d
naseokju <lukas95@naver.com>
2022-11-18 01:01:23 +0900

    post

--- blog-frontend/src/pages/PostPage.js
+++ blog-frontend/src/pages/PostPage.js
@@ -1,5 +1,13 @@
+import HeaderContainer from "../containers/common/HeaderContainer";
+import PostViewerContainer from "../containers/post/PostViewerContainer";
+
 const PostPage = () => {
-    return <div>포스트 읽기</div>;
+    return (
+        <>
+            <HeaderContainer />
+            <PostViewerContainer />
+        </>
+    );
 };
 
 export default PostPage;
