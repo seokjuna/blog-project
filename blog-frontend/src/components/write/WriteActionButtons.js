fa68f55b5911e6536c3386c5ed9f8944ea3921c8
naseokju <lukas95@naver.com>
2022-11-21 17:45:31 +0900

    remove

--- blog-frontend/src/components/write/WriteActionButtons.js
+++ blog-frontend/src/components/write/WriteActionButtons.js
@@ -21,7 +21,7 @@ const WriteActionButtons = ({ onCancel, onPublish, isEdit }) => {
     return (
         <WriteActionButtonsBlock>
             <StyledButton cyan onClick={onPublish}>
-                포스트 등록 {isEdit ? '수정' : '등록'}
+                포스트 {isEdit ? '수정' : '등록'}
             </StyledButton>
             <StyledButton onClick={onCancel}>취소</StyledButton>
         </WriteActionButtonsBlock>
